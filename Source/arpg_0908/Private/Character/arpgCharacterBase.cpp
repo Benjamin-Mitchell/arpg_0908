3a25a6c18c89d49cf959f6f106f83587dc13d672
Benjamin-Mitchell <benjamin.mitchell@live.co.uk>
2024-08-11 14:16:15 +0100

    Fixing tutorial assets

--- Source/arpg_0908/Private/Character/arpgCharacterBase.cpp
+++ Source/arpg_0908/Private/Character/arpgCharacterBase.cpp
@@ -6,7 +6,10 @@
 AarpgCharacterBase::AarpgCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
+	
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AarpgCharacterBase::BeginPlay()
