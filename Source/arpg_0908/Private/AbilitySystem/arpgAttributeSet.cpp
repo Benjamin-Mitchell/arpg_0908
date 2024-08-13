4020de680caf07db3abf7cd919d89b0ea6f38e20
Benjamin-Mitchell <benjamin.mitchell@live.co.uk>
2024-08-13 23:52:15 +0100

    Health and Mana

--- Source/arpg_0908/Private/AbilitySystem/arpgAttributeSet.cpp
+++ Source/arpg_0908/Private/AbilitySystem/arpgAttributeSet.cpp
@@ -2,4 +2,47 @@
 
 
 #include "AbilitySystem/arpgAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UarpgAttributeSet::UarpgAttributeSet()
+{
+	InitHealth(100.0f);
+	InitMaxHealth(100.0f);
+	InitMana(10.0f);
+	InitMaxMana(10.0f);
+}
+
+void UarpgAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//Registry health to be replicated
+	DOREPLIFETIME_CONDITION_NOTIFY(UarpgAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UarpgAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UarpgAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UarpgAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UarpgAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	//Notify the gameplay system that this attribute has been updated.
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UarpgAttributeSet, Health, OldHealth);
+}
+
+void UarpgAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	//Notify the gameplay system that this attribute has been updated.
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UarpgAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UarpgAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	//Notify the gameplay system that this attribute has been updated.
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UarpgAttributeSet, Mana, OldMana);
+}
+
+void UarpgAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	//Notify the gameplay system that this attribute has been updated.
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UarpgAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/arpg_0908/Public/AbilitySystem/arpgAttributeSet.h
+++ Source/arpg_0908/Public/AbilitySystem/arpgAttributeSet.h
@@ -4,8 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "arpgAttributeSet.generated.h"
-
+ 
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+ GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 /**
  * 
  */
@@ -13,5 +19,37 @@ UCLASS()
 class ARPG_0908_API UarpgAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UarpgAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UarpgAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UarpgAttributeSet, MaxHealth);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UarpgAttributeSet, Mana);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UarpgAttributeSet, MaxMana);
 	
+	//By default using the value type itself as a paramter (the only option for parameter), the value will contain the old value.
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;	
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
