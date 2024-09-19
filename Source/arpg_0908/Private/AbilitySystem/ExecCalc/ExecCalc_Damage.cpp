8173f9a57653ed3b6abee1c01187ec176869d424
Benjamin-Mitchell <benjamin.mitchell@live.co.uk>
2024-09-19 15:27:34 +0100

    Critical Hit Chance

--- Source/arpg_0908/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/arpg_0908/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -11,11 +11,17 @@ struct ArpgDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(DamageReduction);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
+
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CritChance);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CritDamageMultiplier);
 	
 	ArpgDamageStatics()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UarpgAttributeSet, DamageReduction, Target, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UarpgAttributeSet, BlockChance, Target, false);
+		
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UarpgAttributeSet, CritChance, Source, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UarpgAttributeSet, CritDamageMultiplier, Source, false);
 	}
 };
 
@@ -29,6 +35,9 @@ UExecCalc_Damage::UExecCalc_Damage()
 {
 	RelevantAttributesToCapture.Add(DamageStatics().DamageReductionDef);
 	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
+	
+	RelevantAttributesToCapture.Add(DamageStatics().CritChanceDef);
+	RelevantAttributesToCapture.Add(DamageStatics().CritDamageMultiplierDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -52,13 +61,30 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	//Get Damage Set By Caller Magnitude
 	float Damage = Spec.GetSetByCallerMagnitude(FArpgGameplayTags::Get().Damage);
 
+	float CritChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().CritChanceDef, EvaluationParameters, CritChance);
+	CritChance = FMath::Max<float>(0.f, CritChance);
+
+	float CritDamageMultiplier = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().CritDamageMultiplierDef, EvaluationParameters, CritDamageMultiplier);
+	//CritDamageMultiplier = FMath::Max<float>(0.f, CritDamageMultiplier); //Crit Damage Multiplier Could be negative and can be greater than 100.0f.
+
+	float CritRand = FMath::RandRange(0.0f, 100.f);
+	if(CritRand < CritChance) 
+	{
+		float DefaultCritMultiplier = 1.5f;
+		//Its a critical hit
+		const float DamageMultiplier = DefaultCritMultiplier + (CritDamageMultiplier/100.f);
+		Damage *= DamageMultiplier;
+	}
+
 	//Capture Block Chance On Target, and determine if there was a successful block
 	//If Block, Halve the damage.
-
 	float BlockChance = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluationParameters, BlockChance);
 	BlockChance = FMath::Max<float>(0.f, BlockChance);
-	
+
+	//Capture Damage Reduction Percentage On Target, and reduce damage by that percentage
 	float DamageReduction = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().DamageReductionDef, EvaluationParameters, DamageReduction);
 	DamageReduction = FMath::Max<float>(0.f, DamageReduction);
